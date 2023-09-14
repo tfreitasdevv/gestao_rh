fff1520679df9e276cb7a3ddab2ffb56e7ce04b2
Thiago Fonseca de Freitas <tfreitasdevv@gmail.com>
2023-09-14 20:54:30 -0300

    Começando CRUD

--- gestao_rh/urls.py
+++ gestao_rh/urls.py
@@ -4,6 +4,7 @@ from django.urls import path, include
 urlpatterns = [
     path('', include('apps.core.urls')),
     path('funcionario/', include('apps.funcionario.urls')),
+    path('empresa/', include('apps.empresa.urls')),
     path('admin/', admin.site.urls),
     path("accounts/", include("django.contrib.auth.urls")),
 ]
