6ebf9988f9ec8d0f0fd2ce83ea837a9e99d8a35e
Thiago Fonseca de Freitas <tfreitasdevv@gmail.com>
2023-09-15 20:45:57 -0300

    Aula 21. Filtrando empresa do func logado

--- apps/funcionario/views.py
+++ apps/funcionario/views.py
@@ -5,3 +5,8 @@ from .models import Funcionario
 
 class FuncionariosList(ListView):
     model = Funcionario
+
+    def get_queryset(self):
+        empresa_logada = self.request.user.funcionario.empresa
+        queryset = Funcionario.objects.filter(empresa=empresa_logada)
+        return queryset
