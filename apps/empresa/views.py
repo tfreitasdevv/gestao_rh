bb43a20ec95c36dcc7f4cde523735caa2da86ba0
Thiago Fonseca de Freitas <tfreitasdevv@gmail.com>
2023-09-15 19:01:57 -0300

    Aula 19

--- apps/empresa/views.py
+++ apps/empresa/views.py
@@ -1,6 +1,6 @@
 # from django.shortcuts import render
 from django.http import HttpResponse
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView
 from .models import Empresa
 
 
@@ -8,7 +8,7 @@ class EmpresaCreate(CreateView):
     model = Empresa
     fields = ['nome']
 
-    # Este método faz com que o funcionário seja atribuído
+    # Este método faz com que o funcionário logado seja atribuído
     # à empresa no momento da criação caso seja válido o form
     def form_valid(self, form):
         obj = form.save()
@@ -16,3 +16,8 @@ class EmpresaCreate(CreateView):
         funcionario.empresa = obj
         funcionario.save()
         return HttpResponse('Ok')
+
+
+class EmpresaEdit(UpdateView):
+    model = Empresa
+    fields = ['nome']
