43d3242db7660958d6f15ba9e6487297942a8ce6
Thiago Fonseca de Freitas <tfreitasdevv@gmail.com>
2023-09-18 17:53:37 -0300

    Aula 26. Departamento Create

--- apps/departamento/urls.py
+++ apps/departamento/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import DepartamentoList
+from .views import DepartamentoList, DepartamentoCreate
 
 urlpatterns = [
     path('list', DepartamentoList.as_view(), name='list_departamentos'),
+    path('novo/',
+         DepartamentoCreate.as_view(), name='create_departamento'),
 ]
--- apps/departamento/views.py
+++ apps/departamento/views.py
@@ -1,4 +1,5 @@
 from django.views.generic import ListView
+from django.views.generic.edit import CreateView
 from .models import Departamento
 
 
@@ -8,3 +9,14 @@ class DepartamentoList(ListView):
     def get_queryset(self):
         empresa_logada = self.request.user.funcionario.empresa
         return Departamento.objects.filter(empresa=empresa_logada)
+
+
+class DepartamentoCreate(CreateView):
+    model = Departamento
+    fields = ['nome']
+
+    def form_valid(self, form):
+        departamento = form.save(commit=False)
+        departamento.empresa = self.request.user.funcionario.empresa
+        departamento.save()
+        return super(DepartamentoCreate, self).form_valid(form)
